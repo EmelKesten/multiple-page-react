97a83bccb39a062c7499e6dc456fafd8d10f963c
Emel <kestenemel@gmail.com>
2022-09-25 22:56:56 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Products from "./routes/products";
+import AboutUs from "./routes/aboutUs";
+import ContactUs from './routes/contactUs';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/products",
+    element: <Products />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/aboutUs",
+    element: <AboutUs />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/contact",
+    element: <ContactUs />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
   );
 }
 
---
+++ src/routes/contactUs.js
@@ -0,0 +1,53 @@
+export default function ContactUs() {
+
+    return (
+      <>
+        <div id="contactUs">
+          <nav className="navbar navbar-expand-lg bg-light">
+            <div className="container-fluid">
+              <a className="navbar-brand" href="/">
+                Home
+              </a>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <a className="nav-link" href="aboutUs">
+                      About Us
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="products">
+                      Products
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link active" href="contact">
+                      Contact Us
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+          <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <h1>Contact Us</h1>
+            </div>
+          </div>
+        </div>
+        </div>
+      </>
+    );
+  }
